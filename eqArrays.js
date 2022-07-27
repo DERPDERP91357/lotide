1d62258fc65db428305e5bca7decf5ba0139c0b3
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-27 00:08:46 +0000

    created eqArray.js

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = function(array1, array2) {
+  let comparison = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      comparison = !comparison;
+    }
+  }
+  return comparison;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 6, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true);
