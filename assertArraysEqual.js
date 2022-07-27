0ab39b864234d9e9725c89278cdaac2921561a28
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-27 00:17:26 +0000

    created new function for equating arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+let eqArrays = function(array1, array2) {
+  let comparison = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      comparison = !comparison;
+    }
+  }
+  return comparison;
+};
+
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 6, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
