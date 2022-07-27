9b6de8f5c6a296c4959035512652f3915404a604
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-27 21:22:57 +0000

    finalized eqObjects function

--- test.js
+++ test.js
@@ -53,22 +53,7 @@ const eqObjects = function(object1, object2) {
   return equality;
 };
 
-//set 1
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-assertEqual(eqObjects(ab, abc), false);
-
-//set 2
 const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
-assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
-assertEqual(eqObjects(cd, cd2), false);
+console.log(eqObjects(cd, cd2));
