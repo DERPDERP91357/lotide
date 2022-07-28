db7d0ff773c5d00e05f6674c89af36710e029f6e
Daniel Suen <danielsuen200791357@gmail.com>
2022-07-28 20:52:19 +0000

    added findKey

---
+++ findKey.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function (object, function1) {
+  let x = ""
+  for (let key in object) {
+    if (function1(object[key])) {
+      x = key;
+      break;
+    }
+  }
+  if (!x) {
+    return undefined;
+  } else {
+    return x;
+  }
+}
+
+
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
