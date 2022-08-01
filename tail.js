f79dd2ac66f88d621f30618941b1b5373e3bfb3b
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-01 18:41:54 +0000

    refactored tail-js

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 let tail = function(array) {
   let x = [];
   if (array.length !== 1 || array.length !== 0) {
@@ -17,15 +8,4 @@ let tail = function(array) {
   return x;
 };
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-console.log(tail([]));
-console.log(tail([1]));
+module.exports = tail;
