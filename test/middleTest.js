2eaa86ecb31326ea8db2845d8cd461fae1b5f8a9
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-01 20:30:59 +0000

    refactored middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,30 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
 //Test cases:
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("middle", () => {
+  it("returns empty array when input is array of length 1", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns empty array when input is array of length 2", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns middle element when input is array of length 3 or more; odd case", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns middle element when input is array of length 3 or more; even case", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,37 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
-console.log(tail([]));
-console.log(tail([1]));
+// console.log(tail([]));
+// console.log(tail([1]));
+
+describe("tail", () => {
+  it("crops array in intended manner", () => {
+    const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    assert.strictEqual(words.length, 2);
+    assert.strictEqual(words[0], "Lighthouse");
+    assert.strictEqual(words[1], "Labs");
+  });
+
+  it("does not change original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("returns empty array when input is array of length 0", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("returns empty array when input is array of length 1", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
